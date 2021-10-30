0a0682265719058e1b252a4c56ca3441d0d73dc7
FahimHassanMollah <fahimhassan0197@gmail.com>
2021-10-30 17:31:08 +0600

    first commit

---
+++ pages/product/mobile.js
@@ -0,0 +1,11 @@
+import styles from '../../styles/Home.module.css'
+
+export default function Mobile() {
+  return (
+    <div className={styles.container}>
+      <h1>Mobile page </h1>
+     
+    </div>
+  )
+}
+
